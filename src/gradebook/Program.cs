2be00ba3edcfbd5e6ca9ef84a62fdaa0c81f0a59
Abhinav Pradeep <Abhinav.Pradeep@hotmail.com>
2019-11-26 12:41:43 +1000

    Added ability to calculate average

--- src/gradebook/Program.cs
+++ src/gradebook/Program.cs
@@ -9,12 +9,14 @@ namespace gradebook
             System.Console.WriteLine("Welcome to the Gradebook app");
             double[,] Grade = new double[5, 4];
             string[] NameArr = new string[5];
+            string [,] CombinedArr = new string[6,4];
+            string [,]AvgArr = new string [5,2];
             for (int i = 0; i <= 4; i++)
             {
                 System.Console.WriteLine("Enter the first name");
                 string Name = Console.ReadLine();
                 NameArr[i] = Name;
-                for (int j = 0; j <= 3; j++)
+                for (int j = 0; j <= 2; j++)
                 {
                     System.Console.WriteLine("Enter subject marks...");
                     string GradeName = Console.ReadLine();
@@ -33,13 +35,35 @@ namespace gradebook
                 }
             }
 
-            for (int i = 0; i < 4;i++)
-            {
-                for (int j = 0; j < 3; j++)
+            for (int i = 0; i <= 4;i++)
+            { 
+                CombinedArr[i,0]=NameArr[i];
+                System.Console.WriteLine(CombinedArr[i,0]);
+                for (int j = 0; j <= 2; j++)
                 {
-                    System.Console.WriteLine(Grade[i, j]);
-                }
+                    String GradeString = Grade[i,j].ToString();
+                    CombinedArr[i+1,j]=GradeString;
+                    System.Console.WriteLine(CombinedArr[i+1,j]);
+                }   
             }
+
+             for (int i = 0; i <= 4;i++)
+            { 
+                AvgArr[i,0]=NameArr[i];
+                System.Console.WriteLine(AvgArr[i,0]);
+                for (int j = 1; j <= 1; j++)
+                {
+                    double num1 =Grade[i,0];
+                    double num2 =Grade[i,1];
+                    double num3 =Grade[i,2];
+                    double add =num1+num2+num3;
+                    double  avg = add/3;
+                    System.Console.WriteLine(avg);
+                }   
+            }
+
+
+
 
         }
     }
